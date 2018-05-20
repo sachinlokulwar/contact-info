b347497898e0861f9935807a04638861e58f3211
Sachin Lokulwar <GS-0997@GSLAB.COM>
2018-05-20 08:20:05 +0530

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name contactInfoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the contactInfoApp
+ */
+angular.module('contactInfoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name contactInfoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the contactInfoApp
+ */
+angular.module('contactInfoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
